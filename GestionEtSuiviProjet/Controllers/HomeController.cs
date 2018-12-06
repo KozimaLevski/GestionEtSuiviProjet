454c9d0db7af8bdc50c5eb7d9bffc2767f274b3c
KozimaLevski <k.aityous.sin@gmail.com>
2018-12-06 18:08:55 +0100

    Premières pages fonctionnelles

--- GestionEtSuiviProjet/Controllers/HomeController.cs
+++ GestionEtSuiviProjet/Controllers/HomeController.cs
@@ -12,7 +12,6 @@ namespace GestionEtSuiviProjet.Controllers
     {
         public ActionResult Index()
         {
-
             IProjetsService ServiceDA = FServiceSgprojet.CreateProjetsService();
 
             // Récupération de la liste des projets
@@ -34,6 +33,48 @@ namespace GestionEtSuiviProjet.Controllers
 
         public ActionResult SuiviProjet()
         {
+            if(Request.Params["idProjet"] != null)
+            {
+                int IDProjet = Convert.ToInt32(Request.Params["idProjet"]);
+                IProjetsService ServiceDA = FServiceSgprojet.CreateProjetsService();
+
+                // Récupération du projet sélectionné
+                Projet ProjetSelect = ServiceDA.GetProjet(IDProjet,true, true, true, true);
+                ViewBag.ProjetSelect = ProjetSelect;
+
+
+                // Récupération des jalons du projet
+                List<Jalon> ListeJalons = ProjetSelect.List_Jalons;
+
+                if (Request.Params["jalonsorderby"] != null)
+                {
+                    int jalonsorderby = Convert.ToInt32(Request.Params["jalonsorderby"]);
+
+                    switch (jalonsorderby)
+                    {
+                        case 1:
+                            // Jalons terminés
+                            Console.WriteLine("Case 1");
+                            break;
+                        case 2:
+                            //Jalons en cours
+                            Console.WriteLine("Case 2");
+                            break; 
+                        case 3:
+                            // Jalons en attente
+                            Console.WriteLine("Case 3");
+                            break;
+                        default:
+                            Console.WriteLine("Default case");
+                            break;
+                    }
+
+                }
+
+                ViewBag.ListeJalons = ListeJalons;
+            }
+
+
             return View();
         }
 
